555ba5bbeaccec5dfaef4d3a66fc53e28331f881
Hypriot Pirate <pirate@hisui.localdomain>
2019-12-01 06:31:27 +0000

    Use environment arguments instead of the build arguments.

--- start_samba_system.sh
+++ start_samba_system.sh
@@ -1,3 +1,12 @@
 #!/bin/ash
+
+# Create an account
+adduser -D $USERNAME
+echo $USERNAME':'$PASSWD | chpasswd
+echo -e $PASSWD'\n'$PASSWD | pdbedit -a -u $USERNAME
+#mkdir /home/$USERNAME/share
+#chown $USERNAME:$USERNAME /home/$USERNAME/share
+
+# Start daemons
 nmbd restart -D
 smbd restart -FS </dev/null
